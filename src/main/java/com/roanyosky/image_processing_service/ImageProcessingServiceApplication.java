dab608809a874ca51d2c30f77e2dd84700902eb9
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-12 20:57:56 +0200

    Add configuration of the dotenv to the main class

--- src/main/java/com/roanyosky/image_processing_service/ImageProcessingServiceApplication.java
+++ src/main/java/com/roanyosky/image_processing_service/ImageProcessingServiceApplication.java
@@ -1,5 +1,6 @@
 package com.roanyosky.image_processing_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ImageProcessingServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> {System.setProperty(entry.getKey(), entry.getValue());});
+
 		SpringApplication.run(ImageProcessingServiceApplication.class, args);
 	}
 
