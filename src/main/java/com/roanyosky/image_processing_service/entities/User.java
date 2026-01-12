fa58b44af8af923caa92a21d4d7f2eb57c1f3514
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-12 21:33:00 +0200

    Create the class User

---
+++ src/main/java/com/roanyosky/image_processing_service/entities/User.java
@@ -0,0 +1,22 @@
+package com.roanyosky.image_processing_service.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "username", nullable = false, length = Integer.MAX_VALUE)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = Integer.MAX_VALUE)
+    private String password;
+}
