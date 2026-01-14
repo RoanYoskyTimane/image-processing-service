6c284cfd50c0bcb5104f8439b349e948e924c664
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-14 23:18:18 +0200

    Add the findYsename query

--- src/main/java/com/roanyosky/image_processing_service/repositories/UserRepository.java
+++ src/main/java/com/roanyosky/image_processing_service/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.roanyosky.image_processing_service.repositories;
 import com.roanyosky.image_processing_service.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User,Integer> {
 
+    User findByUsername(String username);
 }
