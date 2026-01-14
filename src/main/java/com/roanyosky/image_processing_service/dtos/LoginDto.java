8b36c5e46a72e51d37340d976c2194217d24a5cc
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-14 23:24:20 +0200

    Create Dto's for the entity User

---
+++ src/main/java/com/roanyosky/image_processing_service/dtos/LoginDto.java
@@ -0,0 +1,9 @@
+package com.roanyosky.image_processing_service.dtos;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/roanyosky/image_processing_service/dtos/UserCreateDto.java
@@ -0,0 +1,9 @@
+package com.roanyosky.image_processing_service.dtos;
+
+import lombok.Data;
+
+@Data
+public class UserCreateDto {
+    private String username;
+    private String password;
+}
