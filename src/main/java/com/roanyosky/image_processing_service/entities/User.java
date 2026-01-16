719df7d382cfb3a5052776ec1fb20f4e04d81f8a
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-16 12:30:11 +0200

    Implement the interface UserDetails on the class User

--- src/main/java/com/roanyosky/image_processing_service/entities/User.java
+++ src/main/java/com/roanyosky/image_processing_service/entities/User.java
@@ -3,12 +3,17 @@ package com.roanyosky.image_processing_service.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
@@ -19,4 +24,40 @@ public class User {
 
     @Column(name = "password", nullable = false, length = Integer.MAX_VALUE)
     private String password;
+
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of(null);
+    }
+
+    @Override
+    public String getPassword()
+    {
+        return password;
+    }
+
+    @Override
+    public String getUsername()
+    {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
