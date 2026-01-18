c191044d05cc7356a8220f80d23cb317a982af4a
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-18 20:17:31 +0200

    Change the return value from User to Optional

--- src/main/java/com/roanyosky/image_processing_service/repositories/UserRepository.java
+++ src/main/java/com/roanyosky/image_processing_service/repositories/UserRepository.java
@@ -3,8 +3,10 @@ package com.roanyosky.image_processing_service.repositories;
 import com.roanyosky.image_processing_service.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User,Integer> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
