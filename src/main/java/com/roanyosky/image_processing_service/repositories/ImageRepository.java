0f62abafa3830de1e76759b8325a5a401e15a4f6
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-23 22:40:42 +0200

    Add pagination to ImageRepository

--- src/main/java/com/roanyosky/image_processing_service/repositories/ImageRepository.java
+++ src/main/java/com/roanyosky/image_processing_service/repositories/ImageRepository.java
@@ -3,9 +3,10 @@ package com.roanyosky.image_processing_service.repositories;
 import com.roanyosky.image_processing_service.entities.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 import java.util.UUID;
 
 public interface ImageRepository extends JpaRepository<Image, UUID> {
-    List<Image> findImageByOwnerId(Integer ownerId);
+    List<Image> findImageByOwnerId(Integer ownerId, Pageable pageable);
 }
