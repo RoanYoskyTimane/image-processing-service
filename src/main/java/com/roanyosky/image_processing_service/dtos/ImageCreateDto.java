f0c358d558d43bbc3ef6da8566b0cc84603a76ba
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-23 21:51:04 +0200

    Add the tags @Builder, @AllArgsContructor, @NoArgsConstructor to the ImageCreateDto

--- src/main/java/com/roanyosky/image_processing_service/dtos/ImageCreateDto.java
+++ src/main/java/com/roanyosky/image_processing_service/dtos/ImageCreateDto.java
@@ -1,8 +1,14 @@
 package com.roanyosky.image_processing_service.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ImageCreateDto {
     private Integer owner_id;
     private String r2Key;
