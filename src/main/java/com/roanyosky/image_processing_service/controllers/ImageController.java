97e2d481016c42a7fc8baa3e382da888af05b5ea
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-26 22:22:42 +0200

    Implement the RateLimiteService on the upload endpoint

--- src/main/java/com/roanyosky/image_processing_service/controllers/ImageController.java
+++ src/main/java/com/roanyosky/image_processing_service/controllers/ImageController.java
@@ -13,10 +13,13 @@ import com.roanyosky.image_processing_service.filters.SepiaFilter;
 import com.roanyosky.image_processing_service.repositories.ImageRepository;
 import com.roanyosky.image_processing_service.services.ImageService;
 import com.roanyosky.image_processing_service.services.R2Service;
+import com.roanyosky.image_processing_service.services.RateLimiteService;
+import io.github.bucket4j.Bucket;
 import lombok.AllArgsConstructor;
 import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -36,30 +39,40 @@ import java.util.List;
 public class ImageController {
     private final ImageService imageService;
     private final R2Service r2Service;
+    private final RateLimiteService rateLimiteService;
 
     @PostMapping("/upload")
     public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file, @AuthenticationPrincipal User currentUser) {
-        try {
-            //1. Finding measurements
-            HashMap<String, Integer> measures = new HashMap<>();
-            measures = imageService.findingMeasurement(file);
+        //1. Resolves the bucket for this speficic user
+        Bucket bucket = rateLimiteService.resolveBucket((long)currentUser.getId());
 
-            // 2. Upload the raw file to Cloudflare R2 / S3 storage
-            String fileKey = r2Service.uploadFile(file);
+        if (bucket.tryConsume(1))
+        {
+            try {
+                //1. Finding measurements
+                HashMap<String, Integer> measures = new HashMap<>();
+                measures = imageService.findingMeasurement(file);
 
-            // 3. Map file data and metadata to the Data Transfer Object (DTO)
-            ImageCreateDto imageCreateDto = new ImageCreateDto();
-            imageCreateDto.setOwner_id(currentUser.getId());
-            imageCreateDto.setOriginalName(file.getOriginalFilename());
-            imageCreateDto.setR2Key(fileKey);
-            imageCreateDto.setContentType(file.getContentType());
-            imageCreateDto.setFileSize(file.getSize());
-            imageCreateDto.setWidth(measures.get("width"));
-            imageCreateDto.setHeight(measures.get("height"));
-            // 4. Save metadata to the database and return the result
-            return ResponseEntity.ok(imageService.createImage(imageCreateDto));
-        } catch (IOException e) {
-            return ResponseEntity.status(500).body("Upload failed: " + e.getMessage());
+                // 2. Upload the raw file to Cloudflare R2 / S3 storage
+                String fileKey = r2Service.uploadFile(file);
+
+                // 3. Map file data and metadata to the Data Transfer Object (DTO)
+                ImageCreateDto imageCreateDto = new ImageCreateDto();
+                imageCreateDto.setOwner_id(currentUser.getId());
+                imageCreateDto.setOriginalName(file.getOriginalFilename());
+                imageCreateDto.setR2Key(fileKey);
+                imageCreateDto.setContentType(file.getContentType());
+                imageCreateDto.setFileSize(file.getSize());
+                imageCreateDto.setWidth(measures.get("width"));
+                imageCreateDto.setHeight(measures.get("height"));
+                // 4. Save metadata to the database and return the result
+                return ResponseEntity.ok(imageService.createImage(imageCreateDto));
+            } catch (IOException e) {
+                return ResponseEntity.status(500).body("Upload failed: " + e.getMessage());
+            }
+        }else {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body("You have reacheed your free upload limit (2 photos per day)");
         }
     }
 
