257e860f1c67a50a884903ce67f447032f740d7b
Roan Yosky Timane <roanyosky@gmail.com>
2026-02-08 07:54:47 +0200

    Use the SpringFramework domain.Page and domain.Pageable instead of the java.Print

--- src/main/java/com/roanyosky/image_processing_service/repositories/ImageRepository.java
+++ src/main/java/com/roanyosky/image_processing_service/repositories/ImageRepository.java
@@ -3,10 +3,11 @@ package com.roanyosky.image_processing_service.repositories;
 import com.roanyosky.image_processing_service.entities.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.UUID;
 
 public interface ImageRepository extends JpaRepository<Image, UUID> {
-    List<Image> findImageByOwnerId(Integer ownerId, Pageable pageable);
+    Page<Image> findImageByOwnerId(Integer ownerId, Pageable pageable);
 }
